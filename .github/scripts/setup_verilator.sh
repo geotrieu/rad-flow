240215587849a90b4717e71c2563664d8b238788
George Trieu <trieugeorge@gmail.com>
2023-11-21 22:15:49 -0500

    added libraries

--- .github/scripts/setup_verilator.sh
+++ .github/scripts/setup_verilator.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 sudo apt-get update && apt-get upgrade -y
-sudo apt install git help2man perl python3 make autoconf g++ flex bison ccache libgoogle-perftools-dev numactl perl-doc
+sudo apt install git help2man perl python3 make autoconf g++ flex bison ccache libgoogle-perftools-dev numactl perl-doc libfl2 libfl-dev zlibc zlib1g zlib1g-dev
 
 git clone https://github.com/verilator/verilator
-unsetenv VERILATOR_ROOT  # For csh; ignore error if on bash
 unset VERILATOR_ROOT  # For bash
 cd verilator
 autoconf         # Create ./configure script
