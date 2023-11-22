d9d7eba2173cb6cfb5731e0377eff967f70bda1a
George Trieu <trieugeorge@gmail.com>
2023-11-21 22:29:50 -0500

    separated dependencies

--- .github/scripts/setup_verilator.sh
+++ .github/scripts/setup_verilator.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo apt-get update && apt-get upgrade -y
-sudo apt install git help2man perl python3 make autoconf g++ flex bison ccache libgoogle-perftools-dev numactl perl-doc libfl2 libfl-dev zlibc zlib1g zlib1g-dev
+sudo apt install git help2man perl python3 make autoconf g++ flex bison ccache
+sudo apt install libgoogle-perftools-dev numactl perl-doc
+sudo apt install libfl2
+sudo apt install libfl-dev
+sudo apt install zlibc zlib1g zlib1g-dev
 
 git clone https://github.com/verilator/verilator
 unset VERILATOR_ROOT  # For bash
